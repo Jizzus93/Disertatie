63ce25aa96b537dbbe449f58bfa78e4062004514
Ciobanu Octavian <octaku8@yahoo.com>
2018-05-07 12:45:04 +0300

    Backend: VerbPattern.java Finished img generation part Changed the structure of VerbPattern a little so it supports multiple examples for the same pattern Added String generateImageResource(exampleIndex) method to VerbPattern class. It will return the path to png graph representation of the requested Pattern.Example
    VerbEntity.java
    Added addOccurrence method in preparation of reindexing the Search Engine Map to hold <lemma, VerbEntity>. It will bring easier access to eny element in the structure through the VerbEntity object.

--- Backend/src/VerbEntity.java
+++ Backend/src/VerbEntity.java
@@ -11,6 +11,10 @@ public class VerbEntity {
         }
     }
 
+    public void addOccurrence(Occurrence occurrence)
+    {
+
+    }
 
 
 }
--- Backend/src/VerbPattern.java
+++ Backend/src/VerbPattern.java
@@ -14,8 +14,7 @@ import java.util.ArrayList;
 import static org.graphstream.stream.file.FileSinkImages.*;
 
 public class VerbPattern {
-    //graph
-    transient MultiGraph mPattern;
+    transient MultiGraph multiGraph;
     transient XMLReader xmlFileReader = new XMLReader();
     ArrayList<String> arguments = new ArrayList<String>();
     ArrayList<String> adjuncts = new ArrayList<String>();
@@ -25,11 +24,29 @@ public class VerbPattern {
 
     VerbPattern(Occurrence o)
     {
-        mPattern = new MultiGraph("" + o.getTreebankID() + "_" + o.getSentenceID() + "_" + o.getWordID());
+
         examplesNumber = 0;
         addExample(o);
+        createGraph( o , multiGraph);
+        //TODO: GENERATE GRAPH FOR EXAMPLE 1 OR DECOUPLE ARGUMENTS GENERATION
 
-        createGraph(o);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this)
+        {
+            return true;
+        }
+
+        if(!(obj instanceof VerbPattern))
+        {
+            return false;
+        }
+
+        VerbPattern vp = (VerbPattern) obj;
+        //TODO: If the arguments are the same it should be the same pattern
+        return (true);
 
     }
 
@@ -44,34 +61,62 @@ public class VerbPattern {
         return examplesNumber;
     }
 
+    public String generateImageResource(int exampleIndex)
+    {
+        String pathToPNGFile = "graphics/";
+        if(exampleIndex<examplesNumber)
+        {
+            Occurrence occurrence = examples.get(exampleIndex);
+            MultiGraph graphRepresentation = new MultiGraph("" + occurrence.getTreebankID() + "_" + occurrence.getSentenceID() + "_" + occurrence.getWordID());
+            createGraph(occurrence, graphRepresentation);
+            String graphPath = getImageResource(graphRepresentation);
+            if(graphPath.equals("invalidPath"))
+            {
+                pathToPNGFile = "invalidPath";
+            }
+            else
+            {
+                pathToPNGFile += graphPath;
+            }
+        }
+        else
+        {
+            //this is an error case, should not happen unless class is misused
+            pathToPNGFile = "invalidPath";
+        }
 
-    private void createGraph(Occurrence occurrence)
+
+        return pathToPNGFile;
+    }
+
+
+    private void createGraph(Occurrence occurrence, MultiGraph graphRepresentation)
     {
         Sentence a_Sentence = xmlFileReader.getSentence(occurrence);
 
         //adding the verb as the root node
-        mPattern.addNode("Id" + occurrence.getWordID());
+        graphRepresentation.addNode("Id" + occurrence.getWordID());
 
         //make the root verb blue, so it stands out
-        Node root = mPattern.getNode("Id" + occurrence.getWordID());
+        Node root = graphRepresentation.getNode("Id" + occurrence.getWordID());
         root.setAttribute("ui.color", Color.BLUE);
         root.setAttribute("ui.size", "2gu");
         root.addAttribute("ui.label", a_Sentence.getWord(occurrence.getWordID()).getForm());
 
-        createGraphFromRoot(occurrence.getWordID(), a_Sentence, true, true);
+        createGraphFromRoot(occurrence.getWordID(), a_Sentence, true, true, graphRepresentation);
     }
 
 
-    private void createGraphFromRoot(int rootId, Sentence aSentence, boolean addArguments, boolean addAdjuncts)
+    private void createGraphFromRoot(int rootId, Sentence aSentence, boolean addArguments, boolean addAdjuncts, MultiGraph graphRepresentation)
     {
         for(Word w: aSentence.getWordList())
         {
             if(w.getHead() == rootId)
             {
                 String nodeId = "Id" + w.getId();
-                mPattern.addNode(nodeId);
+                graphRepresentation.addNode(nodeId);
 
-                Node currentNode = mPattern.getNode(nodeId);
+                Node currentNode = graphRepresentation.getNode(nodeId);
                 if(addArguments)
                 {
                     arguments.add(w.getDepRel());
@@ -82,48 +127,54 @@ public class VerbPattern {
                     adjuncts.add(w.getDepRel());
                 }
 
-                Node rootNode = mPattern.getNode("Id" + rootId);
+                Node rootNode = graphRepresentation.getNode("Id" + rootId);
 
                 currentNode.addAttribute("ui.label", w.getForm());
-                mPattern.addEdge("Edge"+nodeId, "Id" + rootId, nodeId);
+                graphRepresentation.addEdge("Edge"+nodeId, "Id" + rootId, nodeId);
 
-                Edge currentEdge = mPattern.getEdge("Edge"+nodeId);
+                Edge currentEdge = graphRepresentation.getEdge("Edge"+nodeId);
                 currentEdge.addAttribute("ui.label", w.getDepRel());
 
                 //we should add all the children until there are node left
-                createGraphFromRoot(w.getId(), aSentence, false, false);
+                createGraphFromRoot(w.getId(), aSentence, false, false, graphRepresentation);
             }
         }
     }
 
 
-    public String getImageResource()
+    private String getImageResource(MultiGraph graphRepresentation)
     {
 
         System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         System.setProperty("java.awt.headless","false");
 
-
-        FileSinkImages pic = new FileSinkImages("graphics/" + mPattern.getId() + "/img", OutputType.PNG, Resolutions.HD720, OutputPolicy.ByAttributeEventOutput);
+        String graphPathPrefix = "" + graphRepresentation.getId();
+        FileSinkImages pic = new FileSinkImages("graphics/" + graphRepresentation.getId() + "/img", OutputType.PNG, Resolutions.HD720, OutputPolicy.ByAttributeEventOutput);
         pic.setLayoutPolicy(LayoutPolicy.ComputedInLayoutRunner);
 
 
-        mPattern.clearSinks();
-        mPattern.addSink(pic);
+        graphRepresentation.clearSinks();
+        graphRepresentation.addSink(pic);
 
 
         try {
-            pic.writeAll(mPattern, "sample2.png");
+            pic.writeAll(graphRepresentation, "sample2.png");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        File[] files = new File("graphics/"+graphPathPrefix).listFiles();
+        if(files.length != 0)
+        {
+            graphPathPrefix += "/" + files[files.length-1].getName();
+        }
+        else
+        {
+            //lets hope this doesn't happen
+            graphPathPrefix = "invalidPath";
+        }
 
-        //BUG IN LIB, this works with older versions
-        //TODO: If no fix comes for this bug, an older version of the lib should be used.
-        //mPattern.addAttribute("ui.screenshot", "sample2.png");
-        //Viewer v = mPattern.display();
-        return "sample.png";
+        return graphPathPrefix;
     }
 
 
